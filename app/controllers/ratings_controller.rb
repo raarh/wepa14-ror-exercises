383ca661e67a0f042d5c9841b4a32b2199fb9636
raarh <arto.rantanen@helsinki.fi>
2014-01-18 01:53:19 +0200

    Keskiarvo + exercise1

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,4 +1,27 @@
 class RatingsController < ApplicationController
+  before_action :set_ratings, only: [:show, :edit, :update, :destroy]
   def index
-    end
+    @ratings = Rating.all
+  end
+  def show
+  end
+  def new
+    @ratings = Rating.new
+  end
+  def edit
+  end
+
+
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_ratings
+    @ratings = Rating.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def brewery_params
+    params.require(:brewery).permit(:name, :year)
+  end
+
 end
--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,10 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
+  has_many :ratings
+
+
+  def average_rating
+
+  ratings.average 'score'
+  end
 end
