d92173359403508f4999b73ff29c0be16a7836e6
raarh <arto.rantanen@helsinki.fi>
2014-01-26 15:02:34 +0200

    Hienosäätöä

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,9 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
+    if check_user
+      render action: 'edit'
+    end
   end
 
   # POST /users
@@ -42,24 +45,26 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+      respond_to do |format|
+       if user_params.username.nil? and @user == current_user and @user.update(user_params)
+         format.html { redirect_to @user, notice: 'User was successfully updated.' }
+         format.json { head :no_content }
+       else
+          format.html { render action: 'edit' }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+       end
       end
-    end
   end
 
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
+    if check_user
+      @user.destroy
+      respond_to do |format|
+        format.html { redirect_to users_url }
+        format.json { head :no_content }
+      end
     end
   end
 
@@ -68,7 +73,14 @@ class UsersController < ApplicationController
     def set_user
       @user = User.find(params[:id])
     end
-
+    def check_user
+      if @user == current_user
+           return true
+      else
+        redirect_to @user, notice: 'Error.'
+        return false
+       end
+    end
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:username, :password, :password_confirmation)
