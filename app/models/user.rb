6bc5779c45d5148af0584cc48cdeb43bb2d9ee7b
raarh <arto.rantanen@helsinki.fi>
2014-01-26 13:52:55 +0200

    salasanan validointi

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ActiveRecord::Base
   include RatingAverage
   validates_uniqueness_of :username
   validates_length_of :username, minimum: 3
+  validates_length_of :password, minimum: 3
   validates_length_of :username, maximum: 15
+  validates :password, format: {with: /\d/, message: "At least one number"}
+  validates :password, format: {with: /[A-Z]/, message: "At least one upper-case letter"}
   has_many :memberships, dependent: :destroy
   has_many :beer_clubs, through: :memberships
   has_many :ratings, dependent: :destroy
