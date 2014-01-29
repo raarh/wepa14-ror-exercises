b1966d7d9db24f0c95927d63cad763645445bb21
raarh <arto.rantanen@helsinki.fi>
2014-01-29 23:16:45 +0200

    Validaattori tukee myös tyhjän vuoden

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -5,7 +5,7 @@ class Brewery < ActiveRecord::Base
   has_many :beers, :dependent => :destroy
   has_many :ratings, :through => :beers
   validates :name, :presence => true
-
+  validates :year, numericality: { only_integer: true }
   validates_with YearValidator
 
 
--- app/models/year_validator.rb
+++ app/models/year_validator.rb
@@ -1,9 +1,14 @@
 class YearValidator < ActiveModel::Validator
+
   def validate(record)
-    if record.year > Date.today.year
-      record.errors[:year] << ": must be  <=#{Date.today.year}"
-    elsif record.year <1040
-      record.errors[:year] << ": must be >=1040"
+    begin
+      if record.year > Date.today.year
+        record.errors[:year] << ": must be  <=#{Date.today.year}"
+      elsif record.year <1040
+         record.errors[:year] << ": must be >=1040"
+      end
+    rescue
+      record.errors[:year] << ": Wrong value"
     end
   end
 end
