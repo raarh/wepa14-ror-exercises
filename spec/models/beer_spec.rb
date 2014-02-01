8bd05129f0afdb7dbab4f576f2097f445d4d4f8a
raarh <arto.rantanen@helsinki.fi>
2014-02-01 20:15:58 +0200

    Beer spec

---
+++ spec/models/beer_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Beer do
+  let(:brewery){ Brewery.create name:"Koff", year:1897}
+  it "with name and style is valid" do
+
+    beer = Beer.create name:"Porter", style:"Porter", brewery_id:brewery.id
+    expect(beer).to be_valid
+    beer = Beer.create name:"Porter"
+    expect(beer).not_to be_valid
+    beer = Beer.create style:"Porter"
+    expect(beer).not_to be_valid
+  end
+  it "belongs to a brewery" do
+    beer = Beer.create name:"Porter", style:"Porter", brewery_id: brewery.id
+    beer.brewery.id.should == brewery.id
+
+  end
+end
