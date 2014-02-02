a116de2b3b5d1e31b5cbd5825df579d9dd275120
raarh <arto.rantanen@helsinki.fi>
2014-02-02 11:52:12 +0200

    Beer spec

--- spec/models/beer_spec.rb
+++ spec/models/beer_spec.rb
@@ -10,6 +10,7 @@ describe Beer do
     expect(beer).not_to be_valid
     beer = Beer.create style:"Porter"
     expect(beer).not_to be_valid
+    expect(Beer .count).to eq(1)
   end
   it "belongs to a brewery" do
     beer = Beer.create name:"Porter", style:"Porter", brewery_id: brewery.id
