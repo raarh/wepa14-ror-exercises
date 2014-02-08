9382adf00368716bb75df73be86acf109a494bbf
raarh <arto.rantanen@helsinki.fi>
2014-02-08 18:13:03 +0200

    Tehtävien mukaiseksi palautus

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,7 +1,7 @@
 class Rating < ActiveRecord::Base
   belongs_to :beer
   belongs_to :user
-  validates_uniqueness_of :beer_id, :user_id
+  #validates_uniqueness_of :beer_id, :user_id
   validates_numericality_of :score, { greater_than_or_equal_to: 1,
                                       less_than_or_equal_to: 50,
                                       only_integer: true }
