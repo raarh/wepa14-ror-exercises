07e76751a7ddc87e2b0cdf8dbbc6db7849103121
raarh <arto.rantanen@helsinki.fi>
2014-02-16 15:57:43 +0200

    beermappingApi ja placesControllerin refaktorointi

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,18 +4,10 @@ class PlacesController < ApplicationController
   end
 
   def search
-    api_key = "184cb69a339609dca675cdfa85ddc0ec"
-    url = "http://beermapping.com/webservice/loccity/#{api_key}/"
-    response = HTTParty.get "#{url}#{ERB::Util.url_encode(params[:city])}"
-    places = response.parsed_response["bmp_locations"]["location"]
-
-    if places.is_a?(Hash) and places['id'].nil?
-      redirect_to places_path, :notice => "No places in #{params[:city]}"
+    @places = BeermappingApi.places_in(params[:city])
+    if @places.empty?
+      redirect_to places_path, notice: "No locations in #{params[:city]}"
     else
-      places = [places] if places.is_a?(Hash)
-      @places = places.inject([]) do | set, location|
-        set << Place.new(location)
-      end
       render :index
     end
   end
