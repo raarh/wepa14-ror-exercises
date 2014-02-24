e522441a53a374221ce6a506ec5dd4906b981df5
raarh <arto.rantanen@helsinki.fi>
2014-02-24 15:47:28 +0200

    javascriptillinen beerlist

--- app/assets/javascripts/beerlist.js
+++ app/assets/javascripts/beerlist.js
@@ -1,9 +1,66 @@
-/**
- * Created by arto on 24.2.2014.
- */
-$(document).ready(function () {
+var BEERS = {};
+if ( $("#beertable").length>0 ) {
     $.getJSON('beers.json', function (beers) {
-        oluet = beers
-        $("#beers").html("oluita löytyi "+beers.length);
+        BEERS.list = beers;
+        BEERS.sort_by_name;
+        BEERS.show();
     });
+}
+BEERS.show = function(){
+    $("#beertable tr:gt(0)").remove();
+
+    var table = $("#beertable");
+
+    $.each(BEERS.list, function (index, beer) {
+        table.append('<tr>'
+            +'<td>'+beer['name']+'</td>'
+            +'<td>'+beer['style']['style']+'</td>'
+            +'<td>'+beer['brewery']['name']+'</td>'
+            +'</tr>');
+    });
+};
+
+BEERS.sort_by_name = function(){
+    BEERS.list.sort( function(a,b){
+        return a.name.toUpperCase().localeCompare(b.name.toUpperCase());
+    });
+};
+
+BEERS.sort_by_style = function(){
+    BEERS.list.sort( function(a,b){
+        return a.style.style.toUpperCase().localeCompare(b.style.style.toUpperCase());
+    });
+};
+
+BEERS.sort_by_brewery = function(){
+    BEERS.list.sort( function(a,b){
+        return a.brewery.name.toUpperCase().localeCompare(b.brewery.name.toUpperCase());
+    });
+};
+
+$(document).ready(function () {
+    $("#name").click(function (e) {
+        BEERS.sort_by_name();
+        BEERS.show();
+        e.preventDefault();
+    });
+
+    $("#style").click(function (e) {
+        BEERS.sort_by_style();
+        BEERS.show();
+        e.preventDefault();
+    });
+
+    $("#brewery").click(function (e) {
+        BEERS.sort_by_brewery();
+        BEERS.show();
+        e.preventDefault();
+    });
+
+    $.getJSON('beers.json', function (beers) {
+        BEERS.list = beers;
+        BEERS.sort_by_name();
+        BEERS.show();
+    });
+
 });
