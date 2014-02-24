a2c149c77f681da2689d0477a5f47897346c846b
raarh <arto.rantanen@helsinki.fi>
2014-02-24 17:37:38 +0200

    Uusi speksi: lista järjestetty nimen mukaan

--- spec/features/beerlist_spec.rb
+++ spec/features/beerlist_spec.rb
@@ -1,7 +1,14 @@
 require 'spec_helper'
 
 describe "Beerlist page" do
+  before :all do
+    self.use_transactional_fixtures = false
+    WebMock.disable_net_connect!(allow_localhost:true)
+  end
+
   before :each do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.start
     @brewery1 = FactoryGirl.create(:brewery, name:"Koff")
     @brewery2 = FactoryGirl.create(:brewery, name:"Schlenkerla")
     @brewery3 = FactoryGirl.create(:brewery, name:"Ayinger")
@@ -12,10 +19,21 @@ describe "Beerlist page" do
     @beer2 = FactoryGirl.create(:beer, name:"Fastenbier", brewery:@brewery2, style:@style2)
     @beer3 = FactoryGirl.create(:beer, name:"Lechte Weisse", brewery:@brewery3, style:@style3)
   end
+  after :each do
+    DatabaseCleaner.clean
+  end
 
+  after :all do
+    self.use_transactional_fixtures = true
+  end
   it "shows one known beer", js:true do
     visit beerlist_path
-    save_and_open_page
     expect(page).to have_content "Nikolai"
+  end
+  it "beerlist is ordered by name", js:true do
+    visit beerlist_path
+    find('table').find('tr:nth-child(2)').should have_content('Fastenbier')
+    find('table').find('tr:nth-child(3)').should have_content('Lechte Weisse')
+    find('table').find('tr:nth-child(4)').should have_content('Nikolai')
   end
 end
